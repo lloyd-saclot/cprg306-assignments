0ae45150a162fed8437ce611ec764dd60c792043
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-01-15 16:21:08 -0700

    Update page.js

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -3,8 +3,8 @@ import StudentInfo from "./student-info";
 export default function Page() {
     return (
       <main>
-        <StudentInfo />
         <h1>Shopping List</h1>
+        <StudentInfo />
       </main>
     );
   }
