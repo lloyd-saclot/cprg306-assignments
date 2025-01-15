06e092aff25e61730afa63f41d0796f912df80da
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-01-15 15:42:27 -0700

    Created repository

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <h1>Lloyd Saclot</h1>
+            <Link href="https://github.com/lloyd-saclot?tab=repositories">
+            My repositories
+            </Link>
+        </div>
+
+    );
+}
