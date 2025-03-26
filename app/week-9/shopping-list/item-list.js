43e2e8ac5c157c5bd3235f7348b752805ea5b664
Lloyd-Saclot <164666621+lloyd-saclot@users.noreply.github.com>
2025-03-25 23:36:43 -0600

    Updated Week-9

--- app/week-9/shopping-list/item-list.js
+++ app/week-9/shopping-list/item-list.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from 'react';
-import Item from '../items';
+import Item from './items';
 
 export default function ItemList({ items, onItemSelect }) {
   const [sortBy, setSortBy] = useState('name');
